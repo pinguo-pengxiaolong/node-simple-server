01ee5a9795464eac3c33c4b6f9b62e2fafb2e1b5
Erik Peng <wheelo@163.com>
2018-04-24 10:47:21 +0800

    first commit

---
+++ express/index.js
@@ -0,0 +1,14 @@
+/**
+ * The joke is on me!
+ */
+
+
+const path = require('path')
+const express = require('express')
+const app = express()
+
+
+app.use('/html', express.static(path.join(__dirname, '../html')))
+
+
+app.listen(1337, () => console.log('Example app listening on port 3000!'))
---
+++ readme.md
@@ -0,0 +1,5 @@
+## http-simple-server
+    just for test, never use it in production env
+
+## Boot
+    the visiting url: http://localhost:1337/html/test.html
